828608ec7d5e8cdcfb7d7af1bb9724bd09a5a96f
SHREYA KUMARI <65662514+sh-reya@users.noreply.github.com>
2021-06-25 11:53:26 +0530

    Create Is rectangle.java

---
+++ Math/Is rectangle.java
@@ -0,0 +1,22 @@
+public class Solution 
+{
+    public int solve(int A, int B, int C, int D) 
+    {
+        if(A==B && C==D)
+        {
+            return 1;
+        }
+        else if(A==C && B==D)
+        {
+            return 1;
+        }
+        else if(A==D && C==B)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}

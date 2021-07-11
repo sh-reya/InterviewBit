5acc1dc350c12e6d3ae50f4b6f36c5d1c106ffb4
SHREYA KUMARI <65662514+sh-reya@users.noreply.github.com>
2021-07-11 20:27:48 +0530

    Create merge two sorted list.java

---
+++ LinkedList/merge two sorted list.java
@@ -0,0 +1,24 @@
+public class Solution 
+{
+    public ListNode mergeTwoLists(ListNode A, ListNode B) 
+    {
+        if(A==null)
+        {
+            return B;
+        }
+        if(B==null)
+        {
+            return A;
+        }
+        if(A.val<B.val)
+        {
+            A.next=mergeTwoLists(A.next,B);
+            return A;
+        }
+        else
+        {
+            B.next=mergeTwoLists(A,B.next);
+            return B;
+        }
+    }
+}
